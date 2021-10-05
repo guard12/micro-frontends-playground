fb84c8314221d6ea8380d33049e2e85416894b61
Matus Kollarcik <matus.kollarcik@gmail.com>
2021-10-05 17:17:54 +0200

    Add Navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMemoryHistory, createBrowserHistory } from 'history'
 import App from './app'
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory()
+
+  if(onNavigate) {
+    history.listen(onNavigate)
+  }
+
   ReactDOM.render(
-    <App />,
+    <App history={history}/>,
     el
   )
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location
+
+      if(pathname !== nextPathname) {
+        history.push(nextPathname)
+      }
+    }
+  }
 }
 
 if (process.env.NODE_ENV === 'development') {
-  const el = document.querySelector('#_marketing-dev-root')
-  if (el) {
-    mount(el)
+  const devRoot = document.querySelector('#_marketing-dev-root')
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() })
   }
 }
 
